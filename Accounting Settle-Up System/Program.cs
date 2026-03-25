f47d8242f0b12f524607db0179e81c50aec016f5
unknown <estiaanbritz.eb@gmail.com>
2026-03-25 23:49:03 +0200

    add cors back in

--- Accounting Settle-Up System/Program.cs
+++ Accounting Settle-Up System/Program.cs
@@ -7,6 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
@@ -33,9 +43,6 @@ if (app.Environment.IsDevelopment())
                .WithTheme(ScalarTheme.Mars);
     });
 
-
-
-
     // Dynamic URL Logging
     app.Lifetime.ApplicationStarted.Register(() =>
     {
@@ -56,8 +63,7 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
