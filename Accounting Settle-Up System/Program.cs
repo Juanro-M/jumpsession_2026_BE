72ddf41303c2a56bc53a6c3633552f40ebbd7ba3
Juanro Minne <juanrorr@Juanros-MacBook-Air.local>
2026-03-26 11:22:27 +0200

    latest

--- Accounting Settle-Up System/Program.cs
+++ Accounting Settle-Up System/Program.cs
@@ -7,27 +7,22 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
         policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
-
+builder.Services.AddOpenApi(); 
 builder.Services.AddScoped<IAuthService, AuthService>();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Ensure the database is created and migrated
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -36,44 +31,13 @@ using (var scope = app.Services.CreateScope())
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(c =>
-    {
-        c.RouteTemplate = "openapi/{documentName}.json";
-    });
-
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/openapi/v1.json", "Accounting API v1");
-        c.RoutePrefix = "swagger";
-    });
-
-    app.MapScalarApiReference(options =>
-    {
-        options.WithTitle("Accounting API")
-               .WithTheme(ScalarTheme.Mars);
-    });
-
-    // Dynamic URL Logging
-    app.Lifetime.ApplicationStarted.Register(() =>
-    {
-        var server = app.Services.GetRequiredService<Microsoft.AspNetCore.Hosting.Server.IServer>();
-        var addresses = server.Features.Get<Microsoft.AspNetCore.Hosting.Server.Features.IServerAddressesFeature>()?.Addresses;
-        if (addresses != null)
-        {
-            Console.WriteLine("-------------------------------------------------");
-            foreach (var address in addresses)
-            {
-                Console.WriteLine($"Swagger UI: {address}/swagger");
-                Console.WriteLine($"Scalar API Reference: {address}/scalar/v1");
-            }
-            Console.WriteLine("-------------------------------------------------");
-        }
-    });
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
-
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
