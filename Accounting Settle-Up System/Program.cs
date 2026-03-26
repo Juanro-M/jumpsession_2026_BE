4f127bc54089b2abeb88e51fdbd31eb9ec945b26
Juanro Minne <juanrorr@Juanros-MacBook-Air.local>
2026-03-26 11:25:45 +0200

    urls

--- Accounting Settle-Up System/Program.cs
+++ Accounting Settle-Up System/Program.cs
@@ -35,6 +35,22 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+app.Lifetime.ApplicationStarted.Register(() =>
+    {
+        var server = app.Services.GetRequiredService<Microsoft.AspNetCore.Hosting.Server.IServer>();
+        var addresses = server.Features.Get<Microsoft.AspNetCore.Hosting.Server.Features.IServerAddressesFeature>()?.Addresses;
+        if (addresses != null)
+        {
+            Console.WriteLine("-------------------------------------------------");
+            foreach (var address in addresses)
+            {
+                Console.WriteLine($"Swagger UI: {address}/swagger");
+                Console.WriteLine($"Scalar API Reference: {address}/scalar/v1");
+            }
+            Console.WriteLine("-------------------------------------------------");
+        }
+    });
+
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthorization();
